3e634a34b06f6be4ac797c17418442ea73f1c127
abhibamnote <79627580+abhibamnote@users.noreply.github.com>
2022-06-05 15:30:11 +0530

    Some changes in script

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,7 @@
 function navBtnClicked(){
     document.getElementById('NavlinksHere').classList.toggle('make-display-non');
 }
+
+if(document.getElementById("productPage")){
+    console.log(window.location.pathname);
+}
