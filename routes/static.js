fad30759636df3adcfd4ca009d5c8fa9964948bb
Omkar Bhale <omkarbhale001@gmail.com>
2022-06-05 15:15:56 +0530

    Make static path from root

--- routes/static.js
+++ routes/static.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/products', (req, res) => {
-    res.sendFile('../public/html/products.html')
+    res.sendFile('/public/html/products.html')
 })
 
 module.exports = router
